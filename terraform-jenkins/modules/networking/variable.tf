64558aea362a979fa794028cfe90688b74e03719
summu97 <135579543+summu97@users.noreply.github.com>
2024-05-29 16:56:18 +0530

    Create variable.tf

---
+++ terraform-jenkins/modules/networking/variable.tf
@@ -0,0 +1,34 @@
+variable "project_id" {
+type = string
+default = "sumanth-97"
+}
+
+variable "jenkins-server_region" {
+type = string
+default = "us-west1"
+}
+
+variable "jenkins-server_cidr" {
+type = string
+default = "10.0.1.0/24"
+}
+
+variable "desktop-server_region" {
+type = string
+default = "us-west1"
+}
+
+variable "desktop-server_cidr" {
+type = string
+default = "10.0.2.0/24"
+}
+
+variable "jenkins-server_network_tags" {
+type = string
+default = "jenkins-server"
+}
+
+variable "desktop-server_network_tags" {
+type = string
+default = "desktop-server"
+}
