fe3003089d3b4e6d6fac722ac7e2eaaf0a9df5a2
summu97 <135579543+summu97@users.noreply.github.com>
2024-05-29 16:46:07 +0530

    Create main.tf

---
+++ terraform-desktop/modules/desktop-server/main.tf
@@ -0,0 +1,37 @@
+resource "google_compute_instance" "desktop-server" {
+  name         = "${terraform.workspace}-desktop-server"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      labels = {
+        my_label = "${terraform.workspace}"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "projects/sumanth-97/global/networks/default-vpc"
+    subnetwork = "projects/sumanth-97/regions/us-west1/subnetworks/default-desktop-server-subnet"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = "default-svc@sumanth-97.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+
+  tags = [var.name]  # Add network tags
+
+}
