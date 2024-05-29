3544479eaeddf8fd72a5b25a527ea4632ea8ff83
summu97 <135579543+summu97@users.noreply.github.com>
2024-05-29 16:51:01 +0530

    Create variable.tf

---
+++ terraform-jenkins/jenkins-server/variable.tf
@@ -0,0 +1,19 @@
+variable "machine_type" {
+type = string
+default = "n2-standard-2"
+}
+
+variable "name" {
+type = string
+default = "jenkins-server"
+}
+
+variable "zone" {
+type = string
+default = "us-west1-a"
+}
+
+variable "image" {
+type = string
+default = "ubuntu-os-cloud/ubuntu-2004-lts"
+}
