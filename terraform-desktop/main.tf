455bd0c272463fba39f3b5521c90ca705f73dd33
summu97 <135579543+summu97@users.noreply.github.com>
2024-05-31 10:32:32 +0530

    Update main.tf

--- terraform-desktop/main.tf
+++ terraform-desktop/main.tf
@@ -2,6 +2,13 @@ provider "google" {
   project     = "sumanth-97"
 }
 
+terraform {
+  backend "gcs" {
+    bucket  = "sumanth-state-backup-bucket"
+    prefix  = "terraform/state"
+  }
+}
+
 module "desktop-server" {
 source = "/home/jenkins/workspace/pipeline-1/terraform-desktop/modules/desktop-server"
 }
