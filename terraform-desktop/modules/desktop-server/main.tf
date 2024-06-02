683a108f57d79ad0cd7a136c7374c46852bcbc6d
summu97 <135579543+summu97@users.noreply.github.com>
2024-06-02 19:19:11 +0530

    Update main.tf

--- terraform-desktop/modules/desktop-server/main.tf
+++ terraform-desktop/modules/desktop-server/main.tf
@@ -35,7 +35,7 @@ resource "google_compute_instance" "desktop-server" {
   tags = [var.name]  # Add network tags
 
   metadata = {
-    ssh-keys = "root:${file("/root/key_folder")}"
+    ssh-keys = "root:${file("/root/key_folder/id_rsa.pub")}"
   }
 
   metadata_startup_script = <<-EOF
