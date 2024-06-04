b5d7987c2a4359052309bda0b8563eab54d104a4
summu97 <135579543+summu97@users.noreply.github.com>
2024-06-04 13:02:06 +0530

    Update main.tf

--- terraform-desktop/main.tf
+++ terraform-desktop/main.tf
@@ -10,5 +10,5 @@ terraform {
 }
 
 module "desktop-server" {
-source = "/home/jenkins/workspace/pipeline-1/terraform-desktop/modules/desktop-server"
+source = "/var/lib/jenkins/workspace/pipeline-1/terraform-desktop/modules/desktop-server"
 }
