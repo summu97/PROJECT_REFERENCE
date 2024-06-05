27ce3db28733784d2bdda31acd661a4cc9a6a924
summu97 <135579543+summu97@users.noreply.github.com>
2024-06-05 12:38:31 +0530

    Update main.tf

--- terraform-jenkins/modules/jenkins-server/main.tf
+++ terraform-jenkins/modules/jenkins-server/main.tf
@@ -71,7 +71,7 @@ resource "google_compute_instance" "bastion" {
     sudo apt-get install -y software-properties-common
     sudo add-apt-repository --yes --update ppa:ansible/ansible
     sudo apt-get install -y ansible
-    sudo hostnamectl set-hostname ansible-master
+    sudo hostnamectl set-hostname jenkins-ansible-master
 
   EOF
 }
