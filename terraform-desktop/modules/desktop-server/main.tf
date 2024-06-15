7d7519d1b8d899c2e729a8d9bbd72a71493913c4
summu97 <135579543+summu97@users.noreply.github.com>
2024-06-15 19:04:48 +0530

    Update main.tf

--- terraform-desktop/modules/desktop-server/main.tf
+++ terraform-desktop/modules/desktop-server/main.tf
@@ -41,4 +41,52 @@ resource "google_compute_instance" "desktop-server" {
   metadata = {
     ssh-keys = "root:${file("/var/lib/jenkins/.ssh/id_rsa.pub")}"
   } 
+
+
+
+
+
+resource "google_compute_instance" "apache2" {
+  name         = "apache2"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      labels = {
+        my_label = "apache2"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "projects/sumanth-97/global/networks/custom-vpc"
+    subnetwork = "projects/sumanth-97/regions/us-west1/subnetworks/desktop-server-subnet"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  labels = {
+    apache2 = "true"
+  }
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = "custom-svc@sumanth-97.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+
+  tags = [var.name]  # Add network tags
+
+  metadata = {
+    ssh-keys = "root:${file("/var/lib/jenkins/.ssh/id_rsa.pub")}"
+  } 
 }
